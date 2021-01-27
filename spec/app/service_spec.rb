7edd0ddf90819bb9334b47f7fd0dcd6670b6560b
arimay <arima.yasuhiro@gmail.com>
2021-01-27 18:28:44 +0900

    update: spec

--- spec/app/service_spec.rb
+++ spec/app/service_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe App::Service do
   it "has a version number" do
     expect(App::Service::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
